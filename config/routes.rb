5d16d8676bde07607bddb7a0d519129affbfcda7
jones-py <jones03chimfwembe@gmail.com>
2021-01-12 20:08:24 +0000

    adding user model,controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  get 'sessions/new'
+  get 'sessions/show'
+  resources :users
   resources :tasks
   root "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
