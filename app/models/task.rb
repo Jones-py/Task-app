aba6149ef6e444cadf4b3301da5f1317fd9d4171
jones-py <jones03chimfwembe@gmail.com>
2021-01-21 10:05:01 +0000

    Label CRUD added

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
   default_scope { order(created_at: :desc)}
   belongs_to :user, optional: true
   validates :name, presence: true, uniqueness: true, length: { maximum:50}
   validates :Details, presence: true, length: { maximum: 21}
-
-
 end
